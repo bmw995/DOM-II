8e92cd57021dcb2531caf7cedc5bb90ac76a481d
bmw995 <vimmer995@gmail.com>
2020-03-05 18:42:58 -0500

    another test

--- stretch-assignment/index.js
+++
@@ -1,8 +0,0 @@
-blocksDiv = document.querySelector('.blocks');
-blocks = document.querySelectorAll('.block');
-
-Array.from(blocks).map(block => {
-  block.addEventListener('click', function(e) {
-    blocksDiv.prepend(e.target)
-  })
-})
